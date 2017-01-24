6e4a79ca7099f1765ffb4b2b60d15efc792ace8b
Chase Dunagan <cdunagan@utexas.edu>
2017-01-24 15:28:27 -0700

    add food and calories submit

--- main.bundle.js
+++ main.bundle.js
@@ -42,9 +42,39 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var Food = __webpack_require__(1);
+
+	var someFood = new Food();
+
+/***/ },
+/* 1 */
 /***/ function(module, exports) {
 
-	
+	var foodSubmit = document.getElementById('food-submit');
+	var foodName = document.getElementById('name-field');
+	var calorieNumber = document.getElementById('calorie-field');
+	var allFoods = document.getElementById("all-foods-table");
+
+	foodSubmit.addEventListener('click', function () {
+	  var food = foodName.value;
+	  var calories = calorieNumber.value;
+	  submitFood(food, calories);
+	});
+
+	function submitFood(food, calories) {
+	  var newRow = document.createElement('tr');
+	  var nameCell = document.createElement('td');
+	  nameCell.innerText = food;
+	  var calorieCell = document.createElement('td');
+	  calorieCell.innerText = calories;
+	  var deleteCell = document.createElement('td');
+	  newRow.appendChild(nameCell);
+	  newRow.appendChild(calorieCell);
+	  newRow.appendChild(deleteCell);
+	  allFoods.appendChild(newRow);
+	};
 
 /***/ }
 /******/ ]);
