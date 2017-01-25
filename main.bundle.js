a1fd919cf3c3b69e7e6a775909a5b4deeff74096
Chase Dunagan <cdunagan@utexas.edu>
2017-01-24 17:04:59 -0700

    WIP does not submit if food is null

--- main.bundle.js
+++ main.bundle.js
@@ -46,22 +46,41 @@
 
 	var Food = __webpack_require__(1);
 
-	var someFood = new Food();
+	// var someFood = new Food();
 
 /***/ },
 /* 1 */
 /***/ function(module, exports) {
 
+	// require('bootstrap')
+
 	var foodSubmit = document.getElementById('food-submit');
 	var foodName = document.getElementById('name-field');
 	var calorieNumber = document.getElementById('calorie-field');
 	var allFoods = document.getElementById("all-foods-table");
+	var alertBox = document.getElementById("alert-message");
 
 	foodSubmit.addEventListener('click', function () {
 	  var food = foodName.value;
 	  var calories = calorieNumber.value;
-	  submitFood(food, calories);
+	  if (food === "p") {
+	    // var notEnteredMessage = document.createElement("h4");
+	    // throw new Error("Please Enter a food name");
+	    console.log("Hello, this is working");
+	  } else {
+	    submitFood(food, calories);
+	  }
 	});
+
+	// function storeFood(food, calories) {
+	//
+	//   var foodsJSON = localStorage.getItem('name-field');
+	//   var caloriesJSON = localStorage.getItem('calorie-field');
+	//   if ((foodsJSON === null) || (caloriesJSON === null)){
+	//
+	//   }
+	// }
+
 
 	function submitFood(food, calories) {
 	  var newRow = document.createElement('tr');
