fc6052c082beaecafcb5305a4ce1f738c44d5d07
Noah Berman <noah@noahberman.org>
2017-01-29 14:16:14 -0700

    tests for forgotten item entry with error message

--- test/user-must-enter-all-required-information-for-exercise.js
+++ test/user-must-enter-all-required-information-for-exercise.js
@@ -2,7 +2,7 @@ var assert    = require('chai').assert;
 var webdriver = require('selenium-webdriver');
 var test      = require('selenium-webdriver/testing');
 
-test.describe("user forgets to enter necessary info for foods", function(){
+test.describe("user forgets to enter necessary info for exercise", function(){
   var driver;
   this.timeout(10000);
 
@@ -13,39 +13,39 @@ test.describe("user forgets to enter necessary info for foods", function(){
   });
 
   test.afterEach(function() {
-    driver.get('http://localhost:8080/foods.html');
+    driver.get('http://localhost:8080/exercise.html');
     driver.executeScript('window.localStorage.clear()');
     driver.quit();
   });
 
   test.it("user forgets to enter calories", function() {
-    driver.get("http://localhost:8080/foods.html");
+    driver.get("http://localhost:8080/exercises.html");
 
-    var name = driver.findElement({id: 'name-field'});
-    var calories = driver.findElement({id: 'calorie-field'});
-    var submitButton = driver.findElement({id: 'food-submit'});
+    var name = driver.findElement({id: 'exercise-field'});
+    var calories = driver.findElement({id: 'exercise-calorie-field'});
+    var submitButton = driver.findElement({id: 'exercise-submit'});
 
-    name.sendKeys('Sandwich');
+    name.sendKeys('Run');
     name.getAttribute('value').then(function(value) {
-      assert.equal(value, "Sandwich");
+      assert.equal(value, "Run");
     });
 
     submitButton.click();
-    
+
     driver.findElement({id: 'alert-message'}).getText().then(function(textValue) {
       assert.include(textValue, 'Please enter a calorie amount');
     });
     driver.findElement({css: 'table'}).getText().then(function(textValue) {
-      assert.notInclude(textValue, 'Sandwich');
+      assert.notInclude(textValue, 'Run');
     });
   });
   
-  test.it("user forgets to enter food name", function() {
-    driver.get("http://localhost:8080/foods.html");
+  test.it("user forgets to enter exercise name", function() {
+    driver.get("http://localhost:8080/exercises.html");
 
-    var name = driver.findElement({id: 'name-field'});
-    var calories = driver.findElement({id: 'calorie-field'});
-    var submitButton = driver.findElement({id: 'food-submit'});
+    var name = driver.findElement({id: 'exercise-field'});
+    var calories = driver.findElement({id: 'exercise-calorie-field'});
+    var submitButton = driver.findElement({id: 'exercise-submit'});
 
     calories.sendKeys('100');
     calories.getAttribute('value').then(function(value) {
@@ -55,7 +55,7 @@ test.describe("user forgets to enter necessary info for foods", function(){
     submitButton.click();
 
     driver.findElement({id: 'alert-message'}).getText().then(function(textValue) {
-      assert.include(textValue, 'Please enter a food name');
+      assert.include(textValue, 'Please enter an exercise name');
     });
     driver.findElement({css: 'table'}).getText().then(function(textValue) {
       assert.notInclude(textValue, '100');
