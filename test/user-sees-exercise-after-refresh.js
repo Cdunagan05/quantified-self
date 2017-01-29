48fdbfe6d311cf5a2fd82bb39fe6f2791f4bf107
Noah Berman <noah@noahberman.org>
2017-01-29 14:00:23 -0700

    tests for food and exercise persisting after refresh

---
+++ test/user-sees-exercise-after-refresh.js
@@ -0,0 +1,58 @@
+var assert    = require('chai').assert;
+var webdriver = require('selenium-webdriver');
+var test      = require('selenium-webdriver/testing');
+
+test.describe("user sees exercise on exercises.html after refresh", function(){
+  var driver;
+  this.timeout(10000);
+
+  test.beforeEach(function() {
+    driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+  });
+
+  test.afterEach(function() {
+    driver.get('http://localhost:8080/exercises.html');
+    driver.executeScript('window.localStorage.clear()');
+    driver.quit();
+  });
+
+  test.it("user can see same exercise on exercises.html before and after refresh", function() {
+    driver.get("http://localhost:8080/exercises.html");
+
+    var name = driver.findElement({id: 'exercise-field'});
+    var calories = driver.findElement({id: 'exercise-calorie-field'});
+    var submitButton = driver.findElement({id: 'exercise-submit'});
+
+    name.sendKeys('Jump');
+    name.getAttribute('value').then(function(value) {
+      assert.equal(value, "Jump");
+    });
+
+    calories.sendKeys('14');
+    calories.getAttribute('value').then(function(value) {
+      assert.equal(value, '14');
+    });
+
+    submitButton.click();
+
+    driver.findElement({css: 'table td'}).getText().then(function(textValue) {
+      assert.include(textValue, 'Jump');
+    });
+
+    driver.findElement({css: 'table td:nth-child(2)'}).getText().then(function(textValue) {
+      assert.include(textValue, '14');
+    });
+    
+    driver.navigate().refresh();
+
+    driver.findElement({css: 'table td'}).getText().then(function(textValue) {
+      assert.include(textValue, 'Jump');
+    });
+
+    driver.findElement({css: 'table td:nth-child(2)'}).getText().then(function(textValue) {
+      assert.include(textValue, '14');
+    });
+  });
+});
---
+++ test/user-sees-food-after-refresh.js
@@ -0,0 +1,58 @@
+var assert    = require('chai').assert;
+var webdriver = require('selenium-webdriver');
+var test      = require('selenium-webdriver/testing');
+
+test.describe("user sees food on foods.html after refresh", function(){
+  var driver;
+  this.timeout(10000);
+
+  test.beforeEach(function() {
+    driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+  });
+
+  test.afterEach(function() {
+    driver.get('http://localhost:8080/foods.html');
+    driver.executeScript('window.localStorage.clear()');
+    driver.quit();
+  });
+
+  test.it("user can see same food on foods.html before and after refresh", function() {
+    driver.get("http://localhost:8080/foods.html");
+
+    var name = driver.findElement({id: 'name-field'});
+    var calories = driver.findElement({id: 'calorie-field'});
+    var submitButton = driver.findElement({id: 'food-submit'});
+
+    name.sendKeys('Chicken');
+    name.getAttribute('value').then(function(value) {
+      assert.equal(value, "Chicken");
+    });
+
+    calories.sendKeys('900');
+    calories.getAttribute('value').then(function(value) {
+      assert.equal(value, '900');
+    });
+
+    submitButton.click();
+
+    driver.findElement({css: 'table td'}).getText().then(function(textValue) {
+      assert.include(textValue, 'Chicken');
+    });
+
+    driver.findElement({css: 'table td:nth-child(2)'}).getText().then(function(textValue) {
+      assert.include(textValue, '900');
+    });
+    
+    driver.navigate().refresh();
+    
+    driver.findElement({css: 'table td'}).getText().then(function(textValue) {
+      assert.include(textValue, 'Chicken');
+    });
+
+    driver.findElement({css: 'table td:nth-child(2)'}).getText().then(function(textValue) {
+      assert.include(textValue, '900');
+    });
+  });
+});
