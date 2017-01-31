32912f3f338eacb67f64a74f1444662201f8b85f
Chase Dunagan <cdunagan@utexas.edu>
2017-01-30 17:47:05 -0700

    fix merge

--- main.bundle.js
+++ main.bundle.js
@@ -45,7 +45,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	var Exercise = __webpack_require__(1);
-	__webpack_require__(15);
+	var Food = __webpack_require__(15);
 
 	$(document).ready(function () {
 
